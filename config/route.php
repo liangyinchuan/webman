cf483ed7a6ec30e326f4d226ac6b4e193cdb8c2e
admin <liangyinchuan@babeltime.com>
2022-07-22 14:08:39 +0800

    路由文件独立目录 可独立属性路由文件

--- config/route.php
+++ config/route.php
@@ -1,31 +1,23 @@
 <?php
-/**
- * This file is part of webman.
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the MIT-LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @author    walkor<walkor@workerman.net>
- * @copyright walkor<walkor@workerman.net>
- * @link      http://www.workerman.net/
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
- */
 use Webman\Route;
+use support\Request;
 
-// test router
-Route::any('/lyc',[app\controller\TestController::class,'router']);
-Route::get('/lycget',[app\controller\TestController::class,'routermethod']);
-Route::post('/lycpost',[app\controller\TestController::class,'routermethod']);
-Route::any('/file/{file}',[app\controller\TestController::class,'staticfile']);
-Route::get('/html',[app\controller\TestController::class,'html']);
-Route::get('/session',[app\controller\TestController::class,'sessiontest']);
-Route::get('/delsession',[app\controller\TestController::class,'sessiondel']);
+// 引用 route 中的路由文件
+foreach (glob(base_path() . '/route/*.php') as $filename) {
+include_once $filename;
+}
 
-// router group
-Route::group('/group',function (){
-    Route::any('/lyc',[app\controller\TestController::class,'router']);
-    Route::get('/lycget',[app\controller\TestController::class,'routermethod']);
-    Route::post('/lycpost',[app\controller\TestController::class,'routermethod']);
-    Route::any('/lycjson',[app\controller\TestController::class,'routerjson']);
-})->middleware([app\middleware\MiddleA::class,app\middleware\MiddleB::class]);
+// 回退路由
+Route::fallback(function (Request $request) {
+    $resp = [
+        'status' => 0,
+        'code' => 404,
+        'info' => '404 not found',
+        'data' => [],
+        'now' => time(),
+    ];
+    return json($resp);
+});
+
+// 关闭默认路由
+Route::disableDefaultRoute();
---
+++ route/route.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of webman.
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the MIT-LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author    walkor<walkor@workerman.net>
+ * @copyright walkor<walkor@workerman.net>
+ * @link      http://www.workerman.net/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+use Webman\Route;
+
+// test router
+Route::any('/tt',[app\controller\TestController::class,'tt']);
+Route::any('/lyc',[app\controller\TestController::class,'router']);
+Route::get('/lycget',[app\controller\TestController::class,'routermethod']);
+Route::post('/lycpost',[app\controller\TestController::class,'routermethod']);
+Route::any('/file/{file}',[app\controller\TestController::class,'staticfile']);
+Route::get('/html',[app\controller\TestController::class,'html']);
+Route::get('/session',[app\controller\TestController::class,'sessiontest']);
+Route::get('/delsession',[app\controller\TestController::class,'sessiondel']);
+
+// router group
+Route::group('/group',function (){
+    Route::any('/lyc',[app\controller\TestController::class,'router']);
+    Route::get('/lycget',[app\controller\TestController::class,'routermethod']);
+    Route::post('/lycpost',[app\controller\TestController::class,'routermethod']);
+    Route::any('/lycjson',[app\controller\TestController::class,'routerjson']);
+})->middleware([app\middleware\MiddleA::class,app\middleware\MiddleB::class]);
